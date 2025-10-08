71e50cf7833ac416fa75b7eb19a358a009ab39fa
kleos <36254493+amlitio@users.noreply.github.com>
2025-10-08 03:12:57 -0400

    Update local.js

--- src/storage/local.js
+++ src/storage/local.js
@@ -1,1 +1,10 @@
+import fs from 'fs';
+import path from 'path';
+import { getEnv } from '../../config/env.js';
 
+const env = getEnv();
+
+export async function savePdfAndGetUrlLocal(filename, buffer) {
+  fs.writeFileSync(path.join(env.INTERNAL_FILE_DIR, filename), buffer);
+  return `${env.APP_BASE_URL}/files/${filename}`;
+}
